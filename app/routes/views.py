78e5182ee145c7c09ee04cf90484ab1224e1a724
izconcept <izkevin52@gmail.com>
2018-01-28 05:21:00 -0500

    drinks options

--- app/routes/views.py
+++ app/routes/views.py
@@ -2,6 +2,16 @@ from app import app
 from flask import render_template, session, redirect, request
 
 
+drinks = {
+    'rum_and_coke': {'portions': [0, 7, 1], 'percentage': '0.1', 'amount': 8},
+    'coke': {'portions': [0, 8, 0], 'percentage': '0', 'amount': 8},
+    'rum(shot)': {'portions': [0, 0, 1], 'percentage': '0.4', 'amount': 1},
+    'tequila(shot)': {'portions': [0, 0, 1], 'percentage': '0.4', 'amount': 1},
+    'gin(shot)': {'portions': [1, 0, 0], 'percentage': '0.4', 'amount': 1},
+    'gin_and_tonic': {'portions': [1, 7, 0], 'percentage': '0.1', 'amount': 8},
+}
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', drinks=drinks)
--- app/services/BAC.py
+++ app/services/BAC.py
@@ -2,11 +2,4 @@ from datetime import datetime
 
 
 def calculate_bac(drinks, weight, start_time, gender):
-    alcohol = sum([drink['percentage'] * drink['amount'] for drink in drinks])
-    if gender == 'male':
-        g_constant = .73
-    else:
-        g_constant = .66
-
-    time_elapsed = datetime.now() - start_time
-    return ((alcohol * 5.14) / weight * g_constant) - (0.15 * time_elapsed.hour)
+    return ((sum([drink['percentage'] * drink['amount'] for drink in drinks]) * 5.14) / weight * (.73 if gender == 'male' else .66)) - (0.15 * (datetime.now() - start_time).hour)
