9d0b461f6ab4f25ab17a1435a0e0a1d1d2aaab22
izconcept <izkevin52@gmail.com>
2018-01-28 06:50:39 -0500

    carousal, pics, formatting

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
-    $('.your-class').slick({
+    $('.drink-carousel').slick({
         centerMode: true,
         centerPadding: '60px',
         slidesToShow: 3,
+        prevArrow:"<i class=\"fa fa-arrow-circle-o-left prev\" aria-hidden=\"true\" style=\"font-size: 2em;\"></i>",
+        nextArrow:"<i class=\"fa fa-arrow-circle-o-right next\" aria-hidden=\"true\" style=\"font-size: 2em;\"></i>",
         responsive: [
             {
                 breakpoint: 768,
