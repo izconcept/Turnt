04fd12af6a643365ff1a0deba7751018b2db6820
izconcept <izkevin52@gmail.com>
2018-01-31 13:18:46 -0500

    tracker routing and packaging

--- app/routes/views.py
+++ app/routes/views.py
@@ -1,7 +1,13 @@
 from app import app, drinks
 from flask import render_template, session, redirect, request
+from app.services.BAC import calculate_bac
 
 
 @app.route('/')
 def index():
     return render_template('index.html', drinks=drinks)
+
+
+@app.route('/tracker')
+def tracker():
+    return render_template('bactracker.html', )
